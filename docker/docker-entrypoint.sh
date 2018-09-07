0171c58379590518625c5c4b28121bfb2d5f73c5
ErickSkrauch <erickskrauch@yandex.ru>
2018-09-07 12:23:39 +0300

    Adjust docker-entrypoint configuration

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,7 +5,18 @@ XDEBUG_EXTENSION_FILE="/usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini"
 PHP_PROD_INI="/usr/local/etc/php/conf.d/php.prod.ini"
 PHP_DEV_INI="/usr/local/etc/php/conf.d/php.dev.ini"
 
-if [ "$APP_ENV" = "dev" ] ; then
+APP_ENV=$(echo "${APP_ENV:-prod}")
+APP_DEBUG=$(echo "${APP_DEBUG:-false}" | awk '{print tolower($0)}')
+case "$APP_DEBUG" in
+    "true" | "on" | "yes")
+        IS_DEBUG=1
+        ;;
+    *)
+        IS_DEBUG=0
+        ;;
+esac
+
+if [ "$IS_DEBUG" = 1 ] ; then
     echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > $XDEBUG_EXTENSION_FILE
     mv ${PHP_PROD_INI}{,.disabled} 2> /dev/null || true
     mv ${PHP_DEV_INI}{.disabled,} 2> /dev/null || true
@@ -20,4 +31,4 @@ if [ "$1" = "sh" ] || [ "$1" = "composer" ] || [ "$1" = "php" ] ; then
     exit 0
 fi
 
-exec "$@"
+exec "$@" --debug $IS_DEBUG --app-env $APP_ENV --debug $IS_DEBUG
